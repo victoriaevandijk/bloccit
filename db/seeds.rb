884e4da6b48980c56804acc36dc3e8faf1fa6d34
Torie <victoriaevandijk@gmail.com>
2016-01-24 15:46:29 +0000

    Checkpoint 34 Material~

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,22 @@
 include RandomData
+ 15.times do
+   Topic.create!(
+     name:         RandomData.random_sentence,
+     description:  RandomData.random_paragraph
+   )
+ end
+ topics = Topic.all
  
- # Create Posts
+ 
  50.times do
 
    Post.create!(
-
+     topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
  end
+ 
  posts = Post.all
  
 
@@ -21,5 +29,6 @@ include RandomData
  end
  
  puts "Seed finished"
+ puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
