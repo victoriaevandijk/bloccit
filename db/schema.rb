2c0389f7f7aa8304eaaea53be7f5bd24be369fc8
Torie <victoriaevandijk@gmail.com>
2016-01-30 22:35:26 +0000

    Checkpoint 40 Material

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125233331) do
+ActiveRecord::Schema.define(version: 20160130215727) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -42,25 +42,6 @@ ActiveRecord::Schema.define(version: 20160125233331) do
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "topic_id"
-  end
-
-  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
-
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
@@ -75,6 +56,7 @@ ActiveRecord::Schema.define(version: 20160125233331) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,9 +38,17 @@ include RandomData
    )
  end
  
- user = User.first
- user.update_attributes!(
-   email: 'victoriaevandijk@gmail.com', 
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
  )
  
  puts "Seed finished"
