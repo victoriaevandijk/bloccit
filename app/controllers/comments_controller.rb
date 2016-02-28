995dc3e15877d01f5c37952b7a916dfd4d207e5f
Torie <victoriaevandijk@gmail.com>
2016-02-28 14:12:08 +0000

    Checkpoint 47 Assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,50 +1,52 @@
 class CommentsController < ApplicationController
-   before_action :require_sign_in
-   before_action :authorize_user, only: [:destroy]
- 
-   def create
-     @post = Post.find(params[:post_id])
-     @comment = @post.comments.new(comment_params)
-     @comment.user = current_user
-     @new_comment = Comment.new
- 
-     if @comment.save
-       flash[:notice] = "Comment saved successfully."
-     else
-       flash[:alert] = "Comment failed to save."
-     end
-     
-     respond_to do |format|
-       format.html
-       format.js
-     end
-   end
+  before_action :require_sign_in
+  before_action :authorize_user, only: [:destroy]
 
-   def destroy
-     @post = Post.find(params[:post_id])
-     @comment = @post.comments.find(params[:id])
- 
-     if @comment.destroy
-       flash[:notice] = "Comment was deleted."
-     else
-       flash[:alert] = "Comment couldn't be deleted. Try again."
-     end
-     
-     respond_to do |format|
-       format.html
-       format.js
-     end
-   end
-   
-   private
-   def comment_params
-     params.require(:comment).permit(:body)
-   end
-   def authorize_user
-     comment = Comment.find(params[:id])
-     unless current_user == comment.user || current_user.admin?
-       flash[:alert] = "You do not have permission to delete a comment."
-       redirect_to [comment.post.topic, comment.post]
-     end
-   end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+    @new_comment = Comment.new
+
+    if @comment.save
+      flash[:notice] = 'Comment saved successfully.'
+    else
+      flash[:error] = 'Comment failed to save.'
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    if @comment.destroy
+      flash[:notice] = 'Comment was deleted.'
+    else
+      flash[:error] = 'Comment couldn\'t be deleted. Try again.'
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def authorize_user
+    comment = Comment.find(params[:id])
+    unless current_user == comment.user || current_user.admin?
+      flash[:error] = 'You do not have permission to delete a comment.'
+      redirect_to [comment.post.topic, comment.post]
+    end
+  end
 end
